8e2114f886d1044b0ed099a2b718c29da63c4492
risa <zenzaiumum@gmail.com>
2023-05-30 11:11:37 +0900

    教科書6章まで作成

--- Meirei.java
+++ Meirei.java
@@ -27,5 +27,13 @@ public class Meirei{
 		//配列の内容を表示
 		int[] arr = {3,5,7};
 		System.out.println(Arrays.toString(arr));
+		//配列の昇順ソート
+		Arrays.sort(arr);
+		//部分文字列の作成
+		//文字列.substring(以上,未満)
+		String s = "Java".substring(0,2);//Ja
+		//書式を使って文字列を作成する
+		String fmt = String.format("%d:%s,%.1f",10,"Hello",20,235);
+		System.out.println(fmt);
 	}
 }
